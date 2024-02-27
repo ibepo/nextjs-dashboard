f0873669c69036d4853196c0750b50b1a2f47574
ibepo <kefanbufan@163.com>
2024-02-27 14:17:07 +0800

    init

---
+++ app/ui/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="w-full justify-center">
+      <h1 className="text-3xl text-blue-500">dashboard index</h1>
+    </div>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Barlow, Inter, Lusitana } from 'next/font/google';
+
+export const barlow = Barlow({ weight: ['400', '700'], subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
